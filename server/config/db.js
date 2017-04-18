4522f227fbba4d153ddc4b8accad1b85b086cc0e
Jeremy Welton <jrweltond@yahoo.com>
2017-04-18 10:29:24 -0500

    updated db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: "group2.coj9jg8nqvno.us-east-1.rds.amazonaws.com",
+    host: process.env.DB_HOST,
     user: process.env.DB_U,
     password: process.env.DB_PASS,
-    database: "HairSmoothieBar"
+    database: process.env.DB
 });
 
 exports.pool = pool;
