10ea8c93057c2e6a2b2fa33847fcfb0a4c40dfe7
Rachel Eiting <reiting1@gmail.com>
2017-04-18 07:00:30 -0500

    added info to factories and services and set up blog controller

--- client/js/main.js
+++ client/js/main.js
@@ -1,48 +1,51 @@
-var app = angular.module('HairSmoothieBar', ['HairSmoothieBar.controllers', 'HairSmoothieBar.factories',  'HairSmoothieBar.services', 'ngRoute', 'ngResource']);
+var app = angular.module('HairSmoothieBar', ['HairSmoothieBar.controllers', 'HairSmoothieBar.factories', 'HairSmoothieBar.services', 'ngRoute', 'ngResource']);
 
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
-   $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
 
-   $routeProvider.when('/', {
-       templateUrl: 'views/welcome.html'
-    //    controller: 'WelcomeController'
-   })
-   .when('/services', {
-       templateUrl: 'views/services.html',
-       controller: 'ServicesController'
-   })
-   .when('/services/:id', {
-       templateUrl: 'views/service-detail.html',
-       controller: 'ServiceDetailController'
-   })
-   .when('/products', {
-       templateUrl: 'views/products.html',
-       controller: 'ProductsController'
-   })
-    .when('/products/:id', {
-       templateUrl: 'views/product-detail.html',
-       controller: 'ProductDetailController'
-   })
-   .when('/about', {
-       templateUrl: 'views/about.html',
-    //    controller: 'AboutController'
-   })
-   .when('/appointments', {
-       templateUrl: 'views/appointments.html',
-    //    controller: 'AppointmentsController'
-   })
-      .when('/blog', {
-       templateUrl: 'views/blog.html',
-    //    controller: 'BlogController'
-   })
-      .when('/checkout', {
-       templateUrl: 'views/checkout.html',
-    //    controller: 'CheckoutController'
-   })
-      .when('/developers', {
-       templateUrl: 'views/developers.html',
-    //    controller: 'DevelopersController'
-   })
-   
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/welcome.html'
+            //    controller: 'WelcomeController'
+        })
+        .when('/services', {
+            templateUrl: 'views/services.html',
+            controller: 'ServicesController'
+        })
+        .when('/products', {
+            templateUrl: 'views/products.html',
+            controller: 'ProductsController'
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            //    controller: 'AboutController'
+        })
+        .when('/appointments', {
+            templateUrl: 'views/appointments.html',
+            //    controller: 'AppointmentsController'
+        })
+        .when('/blog', {
+            templateUrl: 'views/blog.html',
+            //    controller: 'BlogController'
+        })
+        .when('/checkout', {
+            templateUrl: 'views/checkout.html',
+            //    controller: 'CheckoutController'
+        })
+        .when('/developers', {
+            templateUrl: 'views/developers.html',
+            //    controller: 'DevelopersController'
+        })
+        .when('/services/:id', {
+            templateUrl: 'views/service-detail.html',
+            controller: 'ServiceDetailController'
+        })
+        .when('/products/:id', {
+            templateUrl: 'views/product-detail.html',
+            controller: 'ProductDetailController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 }]);
